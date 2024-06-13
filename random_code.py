165d6a894bec82a9a571ccedc7571edc6fbf34a6
[Ben Abbott] <147154495+BenAbbott449@users.noreply.github.com>
2024-06-13 15:53:24 -0400

    attempted to add a decorator

--- random_code.py
+++ random_code.py
@@ -1,6 +1,17 @@
 import random
 
+def generate_random_decorator(func):
+    def wrapper(*args, **kwargs):
+        print("Generating random number...")
+        return func(*args, **kwargs)
+    return wrapper
+
 def generate_random_number(start, end):
     return random.randint(start, end)
 
-print(generate_random_number(1, 100))
+generate_random_number = generate_random_decorator(generate_random_number)
+
+print((generate_random_number(1, 100)))
+print((generate_random_number(1, 100)))
+
+
